dce057ed116e9bfa9deacb8d2e9018a5d8d4bc6f
J. Simplicio <jessicasimplicio1@gmail.com>
2018-06-07 03:09:46 -0300

    Resolucao de bug no tratamento de espaco

--- cipher.js
+++ cipher.js
@@ -1,4 +1,4 @@
-function cipher( messageToBeCiphered ) {
+function cipher(messageToBeCiphered) {
 	//var caesarCipherTable = Object.entries(obj);
 	var lengthMessage = messageToBeCiphered.length;
 	var message = '';
@@ -6,53 +6,48 @@ function cipher( messageToBeCiphered ) {
 	var numberFixed = 33;
 	var numberX; //FIND A BETTER NAME!
 
+	for (var i = 0; i < lengthMessage; i++) {
+		//trata espaco
 
-	for ( var i = 0; i < lengthMessage; i++) {
-	  //verifica se é maiúscula
-	  //Trata maiuscula
-	  if (messageToBeCiphered.charAt(i) === (messageToBeCiphered.charAt(i)).toUpperCase() ){
-	  	numberx = 65;
-	  	var letter = messageToBeCiphered.charCodeAt(i) - numberX; //32 ok
-	  	var letterCiphered = String.fromCharCode((letter + numberFixed) % 26 + numberX);
-		message += letterCiphered;
+		if (messageToBeCiphered.charCodeAt(i) != 32) { //verifica se é espaco
+      //console.log(messageToBeCiphered.charCodeAt(i));
 
+ 			//verifica se é maiúscula
 
-	  } else { //Trata minuscula
-	    //console.log('minuscula');
-	    numberX = 97;
-	    var letter = messageToBeCiphered.charCodeAt(i) - numberX; //32 ok
-	  	var letterCiphered = String.fromCharCode((letter + numberFixed) % 26 + numberX);
-		message += letterCiphered;
+			if (
+				messageToBeCiphered.charAt(i) ===
+				messageToBeCiphered.charAt(i).toUpperCase()
+			) { 	//Trata maiuscula
 
-	  }
+				numberX = 65;
+				var letter = messageToBeCiphered.charCodeAt(i) - numberX; 
+				var letterCiphered = String.fromCharCode(
+					(letter + numberFixed) % 26 + numberX
+				);
+				message += letterCiphered;
+
+			} else {
+				//Trata minuscula
+				numberX = 97;
+				var letter = messageToBeCiphered.charCodeAt(i) - numberX;
+				var letterCiphered = String.fromCharCode(
+					(letter + numberFixed) % 26 + numberX
+				);
+				message += letterCiphered;
+				
+			}
+		} else {
+      message += ' ';
+    }
 	}
 	console.log(message);
 
 
-	//MAUISCULA
-	// for(var i = 0; i < lengthMessage; i++) {
-	//   var letter = messageToBeCiphered.charCodeAt(i) - 65; //32 ok
-	//   var letterCiphered = String.fromCharCode((letter + numberFixed) % 26 + 65);
-	//   //console.log(letterASCIICiphered);
-	// }
-
-	//MINUSCULA
-	// for (var i = 0; i < lengthMessage; i++) {
-	// 	var letter = messageToBeCiphered.charCodeAt(i) - 97;
-
-	// 	if (letter != -65) {
-	// 		//tratar espaco
-	// 		var letterCiphered = String.fromCharCode((letter + numberFixed) % 26 + 97);
-	// 		message += letterCiphered;
-	// 	} else {
-	// 		message += ' ';
-	// 	}
-	// }
-	console.log(message);
 }
 
-//usar tabela ASCII mesmo ao inves dessa criada abaixo
-// tratar espaço;
-
-var message = 'cade onde';
+// Falta:
+// -alert() para pedir mensagem
+// -mensagem de erro se inserir numero ou mensagem em branco
+var message = 'A a';
 cipher(message);
+
